57b6cf95f284d00897971bd41d91398b6c8ceddf
Xero kun <elo48858@gmail.com>
2023-05-20 20:21:06 +0100

    `FEAT` Implemented function to print the formatted contents of a boolean array

--- main.py
+++ main.py
@@ -20,13 +20,37 @@ def to_binary_string(n: int) -> str:
 # ---------------------------------------------------------------------------------------------------------
 
 
+def print_two_dm_boolean_array(the_array: [[bool]]) -> None:
+    for row in the_array:
+        for col in row:
+            if col:
+                print("* ", end="")
+
+            else:
+                print(" ", end="")
+
+        print()
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
 def main():
     """For testing"""
     test_x: float = 0.1
     test_y: float = 0.5
 
+    test_two_dm_boolean_array = [
+        [True, False],
+        [False, True],
+        [True, False]
+    ]
+
     print(is_between_zero_and_one(test_x, test_y))
     print(to_binary_string(8))
+    print()
+
+    print_two_dm_boolean_array(test_two_dm_boolean_array)
 
 
 # ---------------------------------------------------------------------------------------------------------
